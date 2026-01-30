484b28282aa92eb0176ebb5b97488c43f2eeed50
Bsmith179 <bsmith179@student.cscc.edu>
2026-01-30 17:42:17 -0500

    Set up view binding in MainActivity.kt

--- app/src/main/java/com/example/quizapplication/MainActivity.kt
+++ app/src/main/java/com/example/quizapplication/MainActivity.kt
@@ -6,21 +6,35 @@
 
 package com.example.quizapplication
 
+import Question
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.quizapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
