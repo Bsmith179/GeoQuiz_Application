8439ef637ee34a231d5a56f986a859048fbbaa4d
Bsmith179 <bsmith179@student.cscc.edu>
2026-02-08 18:17:24 -0500

    Added SavedStateHandle to return user to correct UI after process death.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.activity:activity-ktx:1.12.3")
 }
